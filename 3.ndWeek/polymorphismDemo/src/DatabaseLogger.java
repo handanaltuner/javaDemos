4b90c65962e6a4d18e7cfe5dba528b436f9adaf9
handanaltuner <34207811+handanaltuner@users.noreply.github.com>
2022-10-17 10:18:33 +0300

    Delete 3.ndWeek/polymorphismDemo directory

--- 3.ndWeek/polymorphismDemo/src/DatabaseLogger.java
+++
@@ -1,7 +0,0 @@
-public class DatabaseLogger extends BaseLogger {
-    //BaseLogger'daki message'yi override ederek database'e ekledik
-    //Database'ye eklendi diye yazdık
-    public void log(String message){
-     System.out.println("Logged to database : "+message);
-    }
-}

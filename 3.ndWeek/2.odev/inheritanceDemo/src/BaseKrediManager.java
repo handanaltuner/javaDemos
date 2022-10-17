0d7f579cca686b3653f420b834a7d72b46d68ea7
handanaltuner <34207811+handanaltuner@users.noreply.github.com>
2022-10-17 10:07:27 +0300

    3.nd.2.homework inheritanceDemo
    17.10.2022

---
+++ 3.ndWeek/2.odev/inheritanceDemo/src/BaseKrediManager.java
@@ -0,0 +1,6 @@
+//Base demek Ana-temel anlamındadır. BaseKrediMAnager, KrediManager'lerin temelidir.
+public class BaseKrediManager {
+    public void Hesapla(){
+        System.out.println("Kredi Hesaplandı");
+    }
+}
